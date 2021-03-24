99695768a1abc9f4bd9ec59abf5112b129e856f7
maxwelbarbosaa <maxwelbarbosa70@hotmail.com>
2021-03-23 21:30:29 -0300

    Mudança TelaLogin Design

--- CadastroJanela.cs
+++ CadastroJanela.cs
@@ -16,5 +16,7 @@ namespace TelaLogin
         {
             InitializeComponent();
         }
+        // Teste
+
     }
 }
--- TelaLogin.Designer.cs
+++ TelaLogin.Designer.cs
@@ -41,25 +41,31 @@ namespace TelaLogin
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(90, 48);
+            this.lblTitulo.BackColor = System.Drawing.Color.DarkRed;
+            this.lblTitulo.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(90, 29);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(95, 34);
+            this.lblTitulo.Size = new System.Drawing.Size(116, 42);
             this.lblTitulo.TabIndex = 0;
             this.lblTitulo.Text = "Login";
             // 
             // txbSenha
             // 
+            this.txbSenha.BackColor = System.Drawing.Color.DarkRed;
             this.txbSenha.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbSenha.Location = new System.Drawing.Point(51, 226);
+            this.txbSenha.ForeColor = System.Drawing.Color.Khaki;
+            this.txbSenha.Location = new System.Drawing.Point(51, 237);
             this.txbSenha.Name = "txbSenha";
             this.txbSenha.Size = new System.Drawing.Size(186, 30);
             this.txbSenha.TabIndex = 1;
             // 
             // txbEmail
             // 
+            this.txbEmail.BackColor = System.Drawing.Color.DarkRed;
             this.txbEmail.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbEmail.Location = new System.Drawing.Point(51, 160);
+            this.txbEmail.ForeColor = System.Drawing.Color.Khaki;
+            this.txbEmail.Location = new System.Drawing.Point(51, 153);
             this.txbEmail.Name = "txbEmail";
             this.txbEmail.Size = new System.Drawing.Size(186, 30);
             this.txbEmail.TabIndex = 2;
@@ -67,48 +73,55 @@ namespace TelaLogin
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEmail.Location = new System.Drawing.Point(103, 133);
+            this.lblEmail.Font = new System.Drawing.Font("Intro Rust G Base 2 Line", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.White;
+            this.lblEmail.Location = new System.Drawing.Point(103, 109);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(82, 24);
+            this.lblEmail.Size = new System.Drawing.Size(88, 25);
             this.lblEmail.TabIndex = 4;
             this.lblEmail.Text = "Email:";
             // 
             // lblSenha
             // 
             this.lblSenha.AutoSize = true;
-            this.lblSenha.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSenha.Location = new System.Drawing.Point(103, 199);
+            this.lblSenha.Font = new System.Drawing.Font("Intro Rust G Base 2 Line", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.ForeColor = System.Drawing.Color.White;
+            this.lblSenha.Location = new System.Drawing.Point(103, 198);
             this.lblSenha.Name = "lblSenha";
-            this.lblSenha.Size = new System.Drawing.Size(82, 24);
+            this.lblSenha.Size = new System.Drawing.Size(94, 25);
             this.lblSenha.TabIndex = 5;
             this.lblSenha.Text = "Senha:";
             // 
             // btnEntrar
             // 
-            this.btnEntrar.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.BackColor = System.Drawing.Color.DarkRed;
+            this.btnEntrar.Font = new System.Drawing.Font("Oswald", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.ForeColor = System.Drawing.Color.White;
             this.btnEntrar.Location = new System.Drawing.Point(14, 292);
             this.btnEntrar.Name = "btnEntrar";
             this.btnEntrar.Size = new System.Drawing.Size(121, 35);
             this.btnEntrar.TabIndex = 6;
             this.btnEntrar.Text = "Entrar";
-            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.UseVisualStyleBackColor = false;
             this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
             // 
             // btnCadastrar
             // 
-            this.btnCadastrar.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.BackColor = System.Drawing.Color.DarkRed;
+            this.btnCadastrar.Font = new System.Drawing.Font("Oswald", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.ForeColor = System.Drawing.Color.White;
             this.btnCadastrar.Location = new System.Drawing.Point(146, 292);
             this.btnCadastrar.Name = "btnCadastrar";
             this.btnCadastrar.Size = new System.Drawing.Size(124, 35);
             this.btnCadastrar.TabIndex = 7;
             this.btnCadastrar.Text = "Cadastrar";
-            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.UseVisualStyleBackColor = false;
             // 
             // FormLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(283, 339);
             this.Controls.Add(this.btnCadastrar);
             this.Controls.Add(this.btnEntrar);
